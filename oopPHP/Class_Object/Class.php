83d13d7de6cae9371d9d9774f264beceb53d8162
darisRanggaPratama <daris.rangga.pratama@gmail.com>
2022-12-19 00:00:09 +0700

    initial commit

--- oopPHP/Class_Object/Class.php
+++ oopPHP/Class_Object/Class.php
@@ -1,10 +1,43 @@
 <?php
+// Kelas dengan nama 'Mobil'
+class Mobil
+{
+    // Property atau variabel yang ada pada kelas 'Mobil'
+    public string $merk = "";
+    public string $warna = "";
+    public int $harga = 0;
 
-class Coba {
-    function test(){
-        echo 'coba lagi\n';
+    // Constructor dari kelas 'Mobil'
+    public function __construct(string $merk, string $warna, int $harga = 150000000)
+    {
+        // Mengisi property '$merk' dengan nilai dari parameter '$merk'
+        $this->merk = $merk;
+        // Mengisi property '$warna' dengan nilai dari parameter '$warna'
+        $this->warna = $warna;
+        // Mengisi property '$harga' dengan nilai dari parameter '$harga'
+        $this->harga = $harga;
     }
 
+    final public function move(): void
+    {
+        echo "$this->merk jalan\r\n";
+    }
 }
 
-?>
+// Penjelasan kode
+/*
+ * Dari kode di atas, kita dapat melihat bahwa
+ * constructor = sebuah fungsi yang akan dijalankan
+ * saat sebuah objek dibuat dari sebuah kelas.
+ * Constructor bisa didefinisikan dengan menggunakan kata kunci __construct
+ *  seperti yang terlihat pada baris ke-8.
+ * Pada contoh di atas, constructor menerima 3 parameter yaitu $merk, $warna, dan $harga
+ * yang nantinya akan diisi dengan nilai yang diberikan saat objek dibuat.
+
+* Constructor juga bisa digunakan untuk mengisi nilai default pada property kelas.
+ * Sebagai contoh,
+ * jika kita ingin mengisi property $harga dengan nilai default 100000000
+ * jika tidak ada nilai yang diberikan saat objek dibuat
+ * */
+
+
---
+++ oopPHP/Class_Object/Run.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'Class.php';
+
+// Membuat objek baru dari kelas 'Mobil' dengan mengisi parameter constructor
+// dengan nilai 'Toyota', 'Merah', dan 100000000
+$toyota = new Mobil("Toyota", "Merah");
+$honda = new Mobil("Honda", "Hitam", 300000000);
+
+var_dump($toyota);
+var_dump($honda);
+
+// Menampilkan nilai property '$merk' dari objek '$mobil1'
+echo "$toyota->merk\r\n";
+
+// Menampilkan nilai property '$warna' dari objek '$mobil1'
+echo "$toyota->warna\r\n";
+
+// Menampilkan nilai property '$harga' dari objek '$mobil1'
+echo "$honda->harga\r\n";
+
+$toyota->move();
+
+
