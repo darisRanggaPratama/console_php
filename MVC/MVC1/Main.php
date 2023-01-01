d7b50bcab154192e3752e302c362ef02551d1663
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-01-01 21:08:58 +0700

    initial commit

--- MVC/MVC1/Main.php
+++ MVC/MVC1/Main.php
@@ -1,6 +1,10 @@
 <?php
-require_once 'ProductController.php';
-require_once 'ProductModel.php';
+// Fungsi untuk autoload setiap class. Class & nama file harus sama
+spl_autoload_register(function ($class)
+{
+    require_once __DIR__ . '/library/' . $class . '.php';
+}
+);
 
 $controller = new ProductController();
 $controller->view(1);
