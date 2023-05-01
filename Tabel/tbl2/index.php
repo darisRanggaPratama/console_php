b9737e114c40f06e0fecf05335713ff908e500bc
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-05-01 11:37:14 +0700

    initial commit

--- Tabel/tbl2/index.php
+++ Tabel/tbl2/index.php
@@ -2,37 +2,46 @@
 <html lang="en">
 
 <head>
-    <title>Login</title>
-    <style>
-  .center {
-    text-align: center;
-  }
-</style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="MyStyle.css">
+    <title>Document</title>
 </head>
 
-<body class="center">
-    
-        <form action="loginController.php" method="POST" style="margin-top: 200px;">
-            <h1>Login</h1>
-            <label>Username :</label>
-            <input type="text" name="username" placeholder="input username" required="" autofocus="">
-            <br />
-            <br />
-            <label>Password :</label>
-            <input type="password" name="password" placeholder="input password" required="" />
-            <br />
-            <br />
-
-            <button type="submit">Submit Login</button>
+<body>
+    <div class="box">
+        <div class="form">
+            <h2>
+                SIGN IN
+            </h2>
+            <form action="loginController.php" method="POST">
+                <div class="inputBox">
+                    <input type="text" name="username" required="required" autofocus="">
+                    <span>Username</span>
+                    <i></i>
+                </div>
+                <div class="inputBox">
+                    <input type="password" name="password" required="required">
+                    <span>Password</span>
+                    <i></i>
+                </div>
+            
+            <div class="links">
+                <a href="#">Forgot Password</a>
+                <a href="#">Signup</a>
+            </div>
+            <input type="submit" value="Login">
         </form>
-        <!--Menampung jika ada pesan -->
-        <?php
-        if (isset($_GET['pesan'])) { ?>
-            <label style="color:red;">
-                <?php echo $_GET['pesan']; ?>
-            </label>
-        <?php }  ?>
-    
+             <!--Menampung jika ada pesan -->
+             <?php
+             if (isset($_GET['pesan'])) { ?>
+                 <label style="color:red;">
+                     <?php echo $_GET['pesan']; ?>
+                 </label>
+             <?php }  ?>
+        </div>
+    </div>
 </body>
 
 </html>
