b855f176baa3574160eb2f9676bc170d25d41a10
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-05-12 00:24:11 +0700

    initial commit

---
+++ Tabel/tbl3/fetch_data.php
@@ -0,0 +1,72 @@
+<?php
+
+include('connection.php');
+
+$output = array();
+
+$sql = "SELECT * FROM users";
+
+$totalQuery = mysqli_query($con, $sql);
+$total_all_rows = mysqli_num_rows($totalQuery);
+
+$columns = array(
+    0 => 'id',
+    1 => 'username',
+    2 => 'email',
+    3 => 'mobile',
+    4 => 'city',
+);
+
+// $query = mysqli_query($con, $sql);
+// $count_all_rows = mysqli_num_rows($query);
+
+if (isset($_POST['search']['value'])) {
+    $search_value = $_POST['search']['value'];
+    $sql .= " WHERE username LIKE '%".$search_value."%' ";
+    $sql .= " OR email LIKE '%".$search_value."%' ";
+    $sql .= " OR mobile LIKE '%".$search_value."%' ";
+    $sql .= " OR city LIKE '%".$search_value."%' ";
+}
+
+if (isset($_POST['order'])) {
+    $column_name = $_POST['order'][0]['column'];
+    $order = $_POST['order'][0]['dir'];
+    $sql .= " ORDER BY '".$columns[$column_name]."' ".$order;
+} else {
+    $sql .=" ORDER BY  id ASC";
+}
+
+if ($_POST['length'] != -1) {
+    $start = $_POST['start'];
+    $length = $_POST['length'];
+    $sql .=" LIMIT ".$start.", ".$length;
+}
+
+$data = array();
+
+$query = mysqli_query($con, $sql);
+$count_rows = mysqli_num_rows($query);
+while ($row = mysqli_fetch_assoc($query)) {
+    $sub_array = array();
+    $sub_array[] = $row['id'];
+    $sub_array[] = $row['username'];
+    $sub_array[] = $row['email'];
+    $sub_array[] = $row['mobile'];
+    $sub_array[] = $row['city'];
+    $sub_array[] = '
+    <a href="javascript:void();" data-id="'.$row['id'].'" class="btn btn-sm btn-info edition">Edit</a>
+    <a href="javascript:void();" data-id="'.$row['id'].'" class="btn btn-sm btn-danger deleteBtn">Delete</a>
+    ';
+    $data[] = $sub_array;
+}
+
+$output = array(
+    'data' => $data,
+    'draw' => intval($_POST['draw']),
+    'recordsTotal' =>$count_rows,
+    'recordsFilterred' => $total_all_rows,
+);
+
+echo json_encode($output);
+
+?>
--- Tabel/tbl3/index.php
+++ Tabel/tbl3/index.php
@@ -5,11 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CRUD: USER</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-    rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-    crossorigin="anonymous">
-    <link href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.4/datatables.min.css" rel="stylesheet"/>
- 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.4/datatables.min.css" rel="stylesheet" />
+
+
 
 </head>
 
@@ -22,6 +21,7 @@
                     <div class="col-md-2"></div>
                     <div class="col-md-8">
                         <table id="datatable" class="table">
+                            <caption>Data Master</caption>
                             <thead>
                                 <th>No.</th>
                                 <th>UserName</th>
@@ -31,17 +31,7 @@
                                 <th>Options</th>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Ujang</td>
-                                    <td>ujang@gmail.com</td>
-                                    <td>089999</td>
-                                    <td>Bekasi</td>
-                                    <td>
-                                        <a class="btn">Edit</a>
-                                        <a href="">Delete</a>
-                                    </td>
-                                </tr>
+                                
                             </tbody>
                         </table>
                     </div>
@@ -50,15 +40,27 @@
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
-    integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.4/datatables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script type="text/javascript">
-        $('datatable').DataTable({});
-
+        $('#datatable').DataTable({
+            'serverSide': true,
+            'processing': true,
+            'paging': true,
+            'order': [],
+            'ajax': {
+                'url': 'fetch_data.php',
+                'type': 'post',
+            },
+            'fnCreateRow': function(nRow, aData, iDataIndex) {
+                $(nRow).attr('id', aData[0]);
+            },
+            'columnDefs': [{
+                'target': [0, 5],
+                'orderable': false,
+            }]
+        });
     </script>
 </body>
 
