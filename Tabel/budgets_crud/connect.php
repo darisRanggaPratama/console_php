b77cece2199ab73f5efd99de6ca87c96910feedf
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-06-09 14:56:30 +0700

    initial commit

---
+++ Tabel/budgets_crud/connect.php
@@ -0,0 +1,12 @@
+<?php
+// Data koneksi
+$server = 'localhost';
+$user = 'rangga';
+$password = 'rangga';
+$database = 'test';
+
+// Koneksi ke database
+$connect = mysqli_connect($server, $user, $password, $database);
+
+
+?>
