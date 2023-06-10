5290e2d2d25d2e833162a2380b952e390368ebd7
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-06-10 19:16:53 +0700

    initial commit

--- Chart/Chart11/index.php
+++ Chart/Chart11/index.php
@@ -13,6 +13,8 @@ require_once "model.php";
     <title>Simulasi Payroll</title>
     <!-- Load library Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+ 
     <style>
         canvas {
             padding-left: 0;
@@ -37,10 +39,12 @@ require_once "model.php";
 </head>
 
 <body>
+   
     <form action="index.php" method="GET">
         <p style="text-align: center;">Tahun</p>
-        <label for="tahun1">From</label>
-        <select id="tahun1" name="thn1">
+        <div class="input-group mb-3">
+        <label for="tahun1" class="input-group-text">From</label>
+        <select id="tahun1" name="thn1" class="form-select">
             <?php
             $year = array(2021, 2022, 2023, 2024, 2025);
 
@@ -51,8 +55,10 @@ require_once "model.php";
             ?>
 
         </select>
-        <label for="tahun2">To</label>
-        <select id="tahun2" name="thn2">
+        </div>
+        <div class="input-group mb-3">
+        <label for="tahun2" class="input-group-text">To</label>
+        <select id="tahun2" name="thn2" class="form-select">
             <?php
             for ($i = 0; $i < sizeof($year); $i++) {
                 echo "<option value=" . $year[$i] . ">$year[$i]</option>";
@@ -60,9 +66,11 @@ require_once "model.php";
             ?>
 
         </select>
+        </div>
         <p style="text-align: center;">Bulan</p>
-        <label for="bulan">From</label>
-        <select id="bulan1" name="bln1">
+        <div class="input-group mb-3">
+        <label for="bulan" class="input-group-text">From</label>
+        <select id="bulan1" name="bln1" class="form-select">
             <?php
             $month = array(
                 "01" => "January",
@@ -85,18 +93,21 @@ require_once "model.php";
             ?>
 
         </select>
-        <label for="bulan">To</label>
-        <select id="bulan2" name="bln2">
+        </div>
+        <div class="input-group mb-3">
+        <label for="bulan" class="input-group-text">To</label>
+        <select id="bulan2" name="bln2" class="form-select">
             <?php
             foreach ($month as $key => $value) {
                 echo "<option value=" . $key . ">$value</option>";
             }
             ?>
         </select>
+        </div>
         <br>
         <br>
         <div style="text-align: center;">
-            <input style="text-align:center;" type="submit">
+            <input style="text-align:center;" type="submit" class="form-control">
         </div>
     </form>
     <br>
@@ -182,6 +193,9 @@ require_once "model.php";
             }
         });
     </script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+     
 </body>
 
 </html>
