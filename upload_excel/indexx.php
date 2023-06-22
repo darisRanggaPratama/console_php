8c96e8338c43709d9980c1656283bef137702df4
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-06-23 00:27:05 +0700

    initial commit

--- upload_excel/indexx.php
+++ upload_excel/indexx.php
@@ -3,26 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UPLOAD</title>
+    <title>DOWNLOAD</title>
 </head>
 <body>
-    <h3>
-        Data Siswa Hasil Import
-    </h3>
-    <a href="form.php" >Import Data</a>
-    <br>
-    <br>
+    <h3>DATA SISWA</h3>
+    <a href="process.php">Export ke Excel</a>
+    <br><br>
     <table border="1" cellpadding="5">
         <tr>
-            <th>No</th>
+            <th>NO</th>
             <th>NIS</th>
-            <th>Nama</th>
-            <th>Gender</th>
-            <th>Telepon</th>
-            <th>Alamat</th> 
+            <th>NAMA</th>
+            <th>JENIS KELAMIN</th>
+            <th>TELEPON</th>
+            <th>ALAMAT</th>
         </tr>
         <?php
-        include 'connect.php';
+        include "connect.php";
 
         $query = "SELECT * FROM siswa";
 
@@ -30,18 +27,18 @@
 
         $no = 1;
 
-        while ($data = mysqli_fetch_array($sql)){
-            echo "<tr>";
-            echo "<td>" .$no++. "</td>";
+        while ($data = mysqli_fetch_array($sql)) {
+            echo "<tr>"; 
+            echo "<td>" .$no. "</td>";
             echo "<td>" .$data['nis']. "</td>";
             echo "<td>" .$data['nama']. "</td>";
             echo "<td>" .$data['gender']. "</td>";
             echo "<td>" .$data['telp']. "</td>";
             echo "<td>" .$data['alamat']. "</td>";
             echo "</tr>";
-
+            $no++;
         }
-        ?>
+?>
     </table>
 </body>
 </html>
