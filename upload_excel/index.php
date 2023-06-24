baa7d19e196512d3d99999399b72410022aef705
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-06-24 14:15:52 +0700

    initial commit

--- upload_excel/index.php
+++ upload_excel/index.php
@@ -1,26 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UPLOAD</title>
+      <!-- Load library Bootstrap 5 & datatable-->
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
 </head>
+
 <body>
+    <tr>
+        <td>
+            <a href="form.php">Import Data</a>
+        </td>
+        <td>
+            <a href="process.php">Export ke Excel</a>
+        </td>
+    </tr>
     <h3>
         Data Siswa Hasil Import
     </h3>
-    <a href="form.php" >Import Data</a>
+
     <br>
     <br>
-    <table border="1" cellpadding="5">
-        <tr>
+    <table id="data" class="table table-bordered" width="100%" cellpadding="5" cellspacing="1" border="1">
+        <caption></caption>
+        <thead>
             <th>No</th>
             <th>NIS</th>
             <th>Nama</th>
             <th>Gender</th>
             <th>Telepon</th>
-            <th>Alamat</th> 
-        </tr>
+            <th>Alamat</th>
+        </thead>
+        <tbody>
         <?php
         include 'connect.php';
 
@@ -30,18 +45,27 @@
 
         $no = 1;
 
-        while ($data = mysqli_fetch_array($sql)){
+        while ($data = mysqli_fetch_array($sql)) {
             echo "<tr>";
-            echo "<td>" .$no++. "</td>";
-            echo "<td>" .$data['nis']. "</td>";
-            echo "<td>" .$data['nama']. "</td>";
-            echo "<td>" .$data['gender']. "</td>";
-            echo "<td>" .$data['telp']. "</td>";
-            echo "<td>" .$data['alamat']. "</td>";
+            echo "<td>" . $no++ . "</td>";
+            echo "<td>" . $data['nis'] . "</td>";
+            echo "<td>" . $data['nama'] . "</td>";
+            echo "<td>" . $data['gender'] . "</td>";
+            echo "<td>" . $data['telp'] . "</td>";
+            echo "<td>" . $data['alamat'] . "</td>";
             echo "</tr>";
-
         }
         ?>
+        </tbody>
     </table>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+	<script>
+		$(document).ready(function() {
+			$('#data').DataTable();
+		});
+	</script>
 </body>
+
 </html>
